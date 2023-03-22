1a8d13f35296314d8aebf8ee856e42c41ee88e03
martijnmichiels <martijnmichiels1@gmail.com>
2023-03-22 13:50:06 +0100

    lab5.1.3

---
+++ lab5.1.3.py
@@ -0,0 +1,33 @@
+import time
+import wiringpi
+import sys
+
+def blink(_pin):
+    wiringpi.digitalWrite(_pin, 1)    # Write 1 ( HIGH ) to pin
+    time.sleep(0.5)
+    wiringpi.digitalWrite(_pin, 0)    # Write 1 ( HIGH ) to pin
+    time.sleep(0.5)
+
+
+#SETUP
+print("Start")
+pin = 1
+pin2 = 2
+pin3 = 4
+pin4 = 5
+
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(pin, 1)            # Set pin to mode 1 ( OUTPUT )
+wiringpi.pinMode(pin2, 1)
+wiringpi.pinMode(pin3, 1)
+wiringpi.pinMode(pin4, 1)
+
+#MAIN
+i = 1
+while i == 1:
+    blink(pin)
+    blink(pin2)
+    blink(pin3)
+    blink(pin4)
+#cleanup
+print("Done")
