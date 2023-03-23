82b44d9612beef733b85702c00cc1fd770b510f9
martijnmichiels <martijnmichiels1@gmail.com>
2023-03-23 15:07:04 +0100

    lab5.1.5

--- lab5.1.5.py
+++ lab5.1.5.py
@@ -2,29 +2,40 @@ import time
 import wiringpi
 import sys
 
-def blink(_pin):
-    wiringpi.digitalWrite(_pin, 1)    # Write 1 ( HIGH ) to pin
+def first_blink(groep_1):
+    for pin in groep_1:
+        wiringpi.digitalWrite(pin, 1)    # Write 1 ( HIGH ) to pin
     time.sleep(1)
-    wiringpi.digitalWrite(_pin, 0)    # Write 1 ( HIGH ) to pin
+    for pin in groep_1:
+        wiringpi.digitalWrite(pin, 0)    # Write 1 ( HIGH ) to pin
     time.sleep(1)
 
+def second_blink(groep_2):
+    for pin in groep_2:
+        wiringpi.digitalWrite(pin, 1)    # Write 1 ( HIGH ) to pin
+    time.sleep(1)
+    for pin in groep_2:
+        wiringpi.digitalWrite(pin, 0)    # Write 1 ( HIGH ) to pin
+    time.sleep(1)
 
 #SETUP
 print("Start")
-pin12 = [1, 2]
-pin34 = [3, 4]
+groep_1 = [1, 4]
+groep_2 =[2, 5]
 
 wiringpi.wiringPiSetup()
-wiringpi.pinMode(pin, 1)            # Set pin to mode 1 ( OUTPUT )
-wiringpi.pinMode(pin2, 1)
-wiringpi.pinMode(pin3, 1)
-wiringpi.pinMode(pin4, 1)
+for pin in groep_1:
+    wiringpi.pinMode(pin, 1)            # Set pin to mode 1 ( OUTPUT )
+
+wiringpi.wiringPiSetup()
+for pin in groep_2:
+    wiringpi.pinMode(pin, 1)            # Set pin to mode 1 ( OUTPUT )
 
 #MAIN
 i = 1
 while i == 1:
-    blink(pin1.2) 
-    blink(pin3.4)
+    first_blink(groep_1)
+    second_blink(groep_2)
 
 #cleanup
 print("Done")
